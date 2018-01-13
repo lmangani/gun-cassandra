ea3372a9ec35092ece391badc1fd6ffdf2e1b8b7
root <lorenzo.mangani@gmail.com>
2018-01-13 02:29:28 +0000

    read test

---
+++ test/simple-read.js
@@ -0,0 +1,36 @@
+/* cleanup */
+try{require('fs').unlinkSync('gun.db');
+}catch(e){}
+
+var Gun = require( "gun/gun" );
+var gunNot = require('gun/lib/not')
+var gunDb = require( "../index.js" );
+
+var gun = Gun({
+  file: false // turn off pesky file.js data.json default
+  , db: {
+	contactPoints: ['elassandra-seed'], 
+	keyspace: 'gun_db',
+	drop: false
+  }
+});
+
+// var cat = {name: "Fluffy", species: "kitty"};
+// var mark = {boss: cat};
+// cat.slave = mark;
+// gun.get('mark').put(mark);
+
+console.log('waiting 10 seconds...');
+setTimeout(function(){ 
+	// access the data as if it is a document.
+	gun.get('mark').get('boss').get('name').val(function(data, key){
+	  // `val` grabs the data once, no subscriptions.
+	  console.log("Mark's boss is", data);
+	});
+
+	// traverse a graph of circular references!
+	gun.get('mark').get('boss').get('species').val(function(data, key){
+	  console.log("Boss is a ", data);
+	});
+
+}, 1000);
